e2d76538dc88a61ae9f3f7ae8330410d60219f83
lucas vinicius <lucassilved@gmail.com>
2024-09-25 07:36:45 -0300

    feat: adiciona banco em memoria e em arquivo

--- src/database/InFile.java
+++ src/database/InFile.java
@@ -1,4 +1,18 @@
 package database;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class InFile {
+    private static final String FILE_PATH = "./db.txt";
+
+    public static void Write(final String input) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(FILE_PATH, true))) {
+            writer.write(input);
+            writer.newLine(); // Adiciona uma nova linha após o texto
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- src/database/LA.java
+++ src/database/LA.java
@@ -1,3 +1,5 @@
+package database;
+
 import java.util.ArrayList;
 import java.util.List;
 
