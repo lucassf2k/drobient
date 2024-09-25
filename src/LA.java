cccd566695f354d5cfbb96df383f007adc3da0d0
lucas vinicius <lucassilved@gmail.com>
2024-09-25 07:36:08 -0300

    retirando arquivo de armazenamento dos dados

--- src/LA.java
+++
@@ -1,8 +0,0 @@
-import domain.Drone;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class LA {
-    public static final List<Drone> db = new ArrayList<>();
-}
---
+++ src/database/LA.java
@@ -0,0 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class LA {
+    public static final List<String> db = new ArrayList<>();
+}
